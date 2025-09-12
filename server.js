be49a1fc37d48f858bcf5382b1ee6dc49a5ad6ad
godwinkalu <kalugodwin158@gmail.com>
2025-09-12 23:05:03 +0100

    updatedcustomer

--- server.js
+++ server.js
@@ -3,8 +3,8 @@ require("./config/database")
 const app = express();
 app.use(express.json())
 const PORT = 2000;
-
-
+const customerRouter = require("./router/customersRouter")
+app.use(customerRouter)
 app.listen(PORT, () => {
     console.log(`server is listening to ${PORT}`);
     
