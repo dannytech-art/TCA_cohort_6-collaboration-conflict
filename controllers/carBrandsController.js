081ee513f13bb87d12cadeda087fed6ce63b1f46
Daniel obinna <dobi44134@gmail.com>
2025-09-15 09:09:49 +0100

    add cars

--- controllers/carBrandsController.js
+++ controllers/carBrandsController.js
@@ -1,16 +1,40 @@
-const mongoose = require('mongoose')
-const carmodel = require('../model/carBrandsModel')
+const carBrandmodel = require('../model/carBrandsModel')
+const customermodel = require('../model/customersModel')
+
 
 exports.createCars = async (req,res)=>{
     try {
-        const { carBrand, carModel, dateReleased, customerName, customerIds} = req.body
-        const carExist = carmodel.findOne({carModel, carBrand})
-        if (carExist) {
-            
+        const { carBrand, carModel, dateReleased} = req.body
+        const { id } = req.params
+        const customer = customermodel.findOne(id)
+        if (!customer) {
+            return res.status(404).json({
+            message:"customer does not exist"
+      })
         }
+        
+        const addCar = new carBrandmodel(
+            { 
+                carBrand,
+                carModel, 
+                dateReleased,
+                id, 
+                customerName: customer.customerName,
+                
+            }
+        )
+
+        customer.carIds.push(addCar._id)
+
+        await customer.save()
+        await addCar.save()
+    res.status(201).json({
+        message: `successfully created car`,
+        data: addCar
+    })
 
     } catch (error) {
-        res.status(404).json({
+        res.status(500).json({
             message: `err creating car`
         })
     }
